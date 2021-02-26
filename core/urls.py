70d4345155ca7e7dc140921776afa005b1ab61b9
willykr <yng1106@skku.edu>
2021-02-27 00:51:44 +0900

    eighth commit

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from rooms import views as room_views
 app_name = "core"  # config.urls의 namesapce와 똑같아야 함
 
 urlpatterns = [
-    path("", room_views.all_rooms, name="home"),
+    path("", room_views.HomeView.as_view(), name="home"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    return render(
-        request,
-        "rooms/home.html",
-        context={"page": rooms},
-    )
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    page_kwargs = "potato"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
