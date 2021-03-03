88c8ef731b2c263cb96dfff6c9ba500de815a3e3
willykr <yng1106@skku.edu>
2021-03-04 01:15:56 +0900

    eleventh commit

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,4 +6,5 @@ app_name = "rooms"
 urlpatterns = [
     path("<int:pk>/", views.RoomDetail.as_view(), name="detail"),
     path("<int:pk>/edit/", views.EditRoomView.as_view(), name="edit"),
+    path("search/", views.search, name="search"),
 ]
