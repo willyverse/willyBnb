0b1ef8ac65f99a7f2694d69c7ecd5d81b33067aa
willykr <yng1106@skku.edu>
2021-03-23 23:47:39 +0900

    social login tailwindcss commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      teal: colors.teal,
+    },
     extend: {
       spacing: {
         "25vh": "25vh",
