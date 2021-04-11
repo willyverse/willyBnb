24e9fdac3733d07a1bac40d4566dc4aab82dae2b
willykr <yng1106@skku.edu>
2021-04-12 00:51:26 +0900

    reservation start commit

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,19 @@
+import datetime
 from django.db import models
 from django.utils import timezone  # Go to settings.py TIME_ZONE
 from core import models as core_models
+
+
+class BookedDay(core_models.TimeStampedModel):
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
+
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reservation.room.name + ": " + str(self.day)
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -29,6 +42,9 @@ class Reservation(core_models.TimeStampedModel):
         "rooms.Room", related_name="reservations", on_delete=models.CASCADE
     )
 
+    def __str__(self):
+        return self.room.name + " - " + str(self.check_in) + "~" + str(self.check_out)
+
     def in_progress(self):
         now = timezone.now().date()
         return now >= self.check_in and now <= self.check_out
@@ -40,3 +56,19 @@ class Reservation(core_models.TimeStampedModel):
         return now > self.check_out
 
     is_finished.boolean = True
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            existing_booked_day = BookedDay.objects.filter(
+                reservation__room=self.room, day__range=(start, end)
+            ).exists()
+            if not existing_booked_day:
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                return
+        return super().save(*args, **kwargs)
